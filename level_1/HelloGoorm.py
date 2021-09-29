5f2ae54bb4cbadc7027ec8b79b6b90bcf1f3ed33
Kimwonbin0921 <pransis1@naver.com>
2021-09-29 10:45:38 +0900

    Level 1 - Hello Goorm !

---
+++ level_1/HelloGoorm.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+# 입력한 숫자만큼 'Hello Goorm !' 출력하기
+user_input = int(input())
+m = 0
+
+while m < user_input:
+	print('Hello Goorm !')
+	m += 1
